c71be4e1b71c4e278a9e528412f7370a27b402fc
shriram-walawalkar-123 <149235912+shriram-walawalkar-123@users.noreply.github.com>
2025-01-11 19:51:25 +0530

    added deployed frontend url

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,10 @@ const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://shrifrontend.vercel.app/','http://localhost:3000'],
+}
+));
 // app.use(express.json());
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
