9b89541a8c965df1d72fd579b00801aa2a40757f
shriram-walawalkar-123 <149235912+shriram-walawalkar-123@users.noreply.github.com>
2025-01-11 19:06:36 +0530

    added deployed url of backend

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,3 +1,3 @@
 export const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-deployed-backend-url.com'  // Production URL
-  : 'http://localhost:4000';                 // Development URL
+  ? 'https://shri-h63h.onrender.com'  // Production URL
+  : 'http://localhost:4000';          // Development URL
