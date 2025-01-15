8d136b5e85d9e2bef053342b0052fd51a4d64f7f
shriram-walawalkar-123 <149235912+shriram-walawalkar-123@users.noreply.github.com>
2025-01-16 00:19:30 +0530

    change cors

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,4 +1,4 @@
 console.log("baseurl is : ",process.env.NODE_ENV);
 export const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://shriram-lmtc.onrender.com'  // Production URL
+  ? 'https://shri-h63h.onrender.com'  // Production URL
   : 'http://localhost:4000';          // Development URL
